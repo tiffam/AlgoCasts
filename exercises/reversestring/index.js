fb06b274bffdd22cc88ae6ff1466810318e9cc08
alice <alesialim@gmail.com>
2018-12-09 10:47:09 +0800

    Refactor solution 3 for reversestring exercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,9 +7,7 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  return str.split("").reduce((reverse, character) => {
-    return character + reverse;
-  });
+  return str.split("").reduce((rev, char) => char + rev);
 }
 
 module.exports = reverse;
